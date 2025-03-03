57ae65a7b524a668eb9fcfedaa2dbfa91c53ac6f
Matthias Hölzl <tc@xantira.com>
2025-03-03 06:25:52 +0100

    Add slides for intro videos

---
+++ Folien/Python/Completed/01 Einführung in Software Engineering und Kursüberblick/02 Die Rolle des Software-Engineers.py
@@ -0,0 +1,101 @@
+# %% [markdown]
+#
+# <div style="text-align:center; font-size:200%;">
+#  <b>Die Rolle des Software-Engineers</b>
+# </div>
+# <br/>
+# <div style="text-align:center;">Dr. Matthias Hölzl</div>
+# <br/>
+
+# %% [markdown]
+#
+# # Die Rolle des Software-Engineers
+# ## Mehr als Programmieren: Wertschöpfung durch Software
+
+# %% [markdown]
+#
+# ## I. Einführung
+#
+# **Wer ist ein Software-Engineer?**
+# - Fachkraft, die Ingenieursprinzipien auf Software anwendet
+# - Verbindet technische Umsetzung mit Geschäftsanforderungen
+# - Balanciert Innovation mit Praktikabilität
+# - Programmierung ist eine Kernkompetenz, aber nicht die einzige
+
+# %% [markdown]
+#
+# ## II. Kernaufgaben
+#
+# **Wesentliche Funktionen eines Software-Engineers:**
+#
+# - Anforderungsanalyse und -spezifikation
+# - Softwaredesign und -architektur
+# - Implementierung und Programmierung
+# - Testen und Qualitätssicherung
+# - Wartung und Weiterentwicklung
+# - Dokumentation und Kommunikation
+# - Risikobewertung und -minderung
+
+# %% [markdown]
+#
+# ## III. Spezialisierungsbereiche
+#
+# **Software-Engineers spezialisieren sich oft auf:**
+#
+# - Frontend/UI-Entwicklung
+# - Backend/Systementwicklung
+# - Datenbankdesign und -management
+# - DevOps und SRE
+# - Mobile Anwendungsentwicklung
+# - Eingebettete Systeme
+# - KI/ML-Engineering
+# - Sicherheitstechnik
+# - Spieleentwicklung
+
+# %% [markdown]
+#
+# ## IV. Organisatorischer Kontext
+#
+# **Software-Engineers arbeiten in verschiedenen Umgebungen:**
+#
+# - Produktunternehmen
+# - IT-Dienstleister und Beratungsunternehmen
+# - Startups und Scale-ups
+# - IT-Abteilungen von Großunternehmen
+# - Regierungs- und öffentlicher Sektor
+# - Gemeinnützige und akademische Einrichtungen
+# - Freiberufliche und unabhängige Beratung
+
+# %% [markdown]
+#
+# ## V. Karrierewege
+#
+# **Entwicklung einer Software-Engineering-Karriere:**
+#
+# - Junior Engineer → Engineer → Senior Engineer
+# - Technische Pfade: Principal Engineer, Architekt, Technical Fellow
+# - Management-Pfade: Team Lead, Engineering Manager, CTO
+# - Spezialistenpfade: Domänenexperte, Technischer Spezialist
+# - Angrenzende Rollen: Produktmanagement, DevOps, Data Science
+# - Unternehmerische Wege: Technical Founder, Freiberuflicher Consultant
+
+# %% [markdown]
+#
+# ## VI. Jenseits technischer Fähigkeiten
+#
+# **Wesentliche nicht-technische Fähigkeiten:**
+#
+# - Kommunikation und Zusammenarbeit
+# - Kritisches Denken und Problemlösung
+# - Zeit- und Prioritätsmanagement
+# - Geschäfts- und Domänenverständnis
+# - Anpassungsfähigkeit und kontinuierliches Lernen
+# - Mentoring und Wissensaustausch
+# - Ethische Entscheidungsfindung
+
+# %% [markdown]
+#
+# **Fragen zur Reflexion**
+#
+# - Welche Aspekte des Software-Engineerings finden Sie am interessantesten?
+# - Welche der Karrierewege oder Spezialisierungsbereiche entsprechen Ihren Zielen?
--- Folien/Python/Completed/01 Einführung in Software Engineering und Kursüberblick/03 Überblick über den Kurs.py
+++ Folien/Python/Completed/01 Einführung in Software Engineering und Kursüberblick/03 Überblick über den Kurs.py
@@ -6,3 +6,206 @@
 # <br/>
 # <div style="text-align:center;">Dr. Matthias Hölzl</div>
 # <br/>
+
+# %% [markdown]
+#
+# ## Einführung in Software Engineering und Grundlagen (40 UE)
+#
+# ### Woche 1: Einführung, Überblick und Software
+#
+# - Einführung in Software Engineering und Kursüberblick (8 UE)
+# - Einführung in die verwendete Entwicklungsumgebung (16 UE)
+# - Versionskontrolle mit Git und Kommandozeilennutzung (16 UE)
+
+# %% [markdown]
+#
+# ## Programmiergrundlagen (120 UE)
+#
+# ### Woche 2: Grundlegende Programmierkonzepte und Datentypen
+#
+# - Syntax und grundlegende Datentypen (14 UE)
+# - Kontrollstrukturen und Funktionen/Methoden (26 UE)
+
+# %% [markdown]
+#
+# ### Woche 3: Sequenzen und andere Datenstrukturen
+#
+# - Sequenzen (20 UE)
+# - Weitere Datenstrukturen (20 UE)
+
+# %% [markdown]
+#
+# ### Woche 4: Fehlerbehandlung, Ein/Ausgabe, Kontrollstrukturen
+#
+# - Fehlerbehandlung (16 UE)
+# - Dateien und I/O-Operationen (12 UE)
+# - Fortgeschrittene Kontrollstrukturen (12 UE)
+
+# %% [markdown]
+#
+# ## Objektorientierung (120 UE)
+#
+# ### Woche 5: Einführung in objektorientierte Konzepte
+#
+# - Klassen, Objekte und Methoden (24 UE)
+# - Kapselung und Zugriffsmodifikatoren (16 UE)
+
+# %% [markdown]
+#
+# ### Woche 6: Vererbung, Polymorphie und fortgeschrittene OOP-Konzepte
+#
+# - Vererbung und Polymorphie (28 UE)
+# - Fortgeschrittene OOP-Konzepte (12 UE)
+
+# %% [markdown]
+#
+# ### Woche 7: Design Patterns und Best Practices in OOP
+#
+# - Creational, Structural und Behavioral Patterns (40 UE)
+
+# %% [markdown]
+#
+# ## Testen von Software (80 UE)
+#
+# ### Woche 8: Grundlagen des Testens, Unit-Tests und TDD
+#
+# - Einführung ins Testen und Testpyramide (8 UE)
+# - Unit-Tests und Test Frameworks (20 UE)
+# - Einführung in Test-Driven Development (12 UE)
+
+# %% [markdown]
+#
+# ### Woche 9: Integrationstests und Fortgeschrittene Testkonzepte
+#
+# - Integrationstests (8 UE)
+# - Behavior-Driven Development und Property-Based Testing (12 UE)
+# - Last-, Performanz- und Sicherheitstests (20 UE)
+
+# %% [markdown]
+#
+# ## Grundlagen des Software-Engineerings (160 UE)
+#
+# ### Woche 10: Klassische Entwicklungsprozesse und Modellierung
+#
+# - Überblick über Softwareentwicklungsprozesse (10 UE)
+# - UML-Modellierung: Klassen-, Sequenzdiagramme und Zustandsmaschinen (30 UE)
+
+# %% [markdown]
+#
+# ### Woche 11: Agile Entwicklung, Scrum und XP
+#
+# - Agile Methoden und Scrum (22 UE)
+# - Extreme Programming und CI/CD (18 UE)
+
+# %% [markdown]
+#
+# ### Woche 12: Anforderungsanalyse und Projektmanagement
+#
+# - Anforderungsanalyse und User Stories (26 UE)
+# - Projektmanagement und Risikomanagement (14 UE)
+
+# %% [markdown]
+#
+# ### Woche 13: Softwarearchitektur und Design
+#
+# - Einführung in Softwarearchitektur und -design (8 UE)
+# - Architekturstile und -muster (32 UE)
+
+# %% [markdown]
+#
+# ## Fortgeschrittene Programmierkonzepte (80 UE)
+#
+# ### Woche 14: Fortgeschrittene Funktionen und Module
+#
+# - Fortgeschrittene Funktionskonzepte (16 UE)
+# - Module und Packages (24 UE)
+
+# %% [markdown]
+#
+# ### Woche 15: Generische Programmierung und Metaprogrammierung
+#
+# - Generische Programmierung (18 UE)
+# - Metaprogrammierung (22 UE)
+
+# %% [markdown]
+#
+# ## Clean Code und Clean Architektur (120 UE)
+#
+# ### Woche 16: Clean Code und Refactoring
+#
+# - Clean Code (16 UE)
+# - Code Smells (4 UE)
+# - Refactoring Katalog (8 UE)
+# - Refactoring Praxisbeispiel (12 UE)
+
+# %% [markdown]
+#
+# ### Woche 17: Einführung in SOLID und GRASP Prinzipien
+#
+# - Einführung in SOLID und GRASP (32 UE)
+# - GRASP Prinzipien (8 UE)
+
+# %% [markdown]
+#
+# ### Woche 18: Vertiefung der SOLID und GRASP Prinzipien
+#
+# - Weitere GRASP Patterns (20 UE)
+# - Anwendung von SOLID und GRASP (20 UE)
+
+# %% [markdown]
+#
+# ## Datenbanken: SQL und NoSQL (120 UE)
+#
+# ### Woche 19: Relationale Datenbanken und SQL
+#
+# - Einführung in relationale Datenbanken (2 UE)
+# - SQL-Grundlagen: SELECT, INSERT, UPDATE, DELETE (18 UE)
+# - Fortgeschrittene SQL-Konzepte: Joins, Subqueries, Indexierung (20 UE)
+
+# %% [markdown]
+#
+# ### Woche 20: Datenbankdesign, Datenbankanbindung und ORM
+#
+# - Datenbankdesign und Normalisierung (12 UE)
+# - Datenbankprogrammierung (16 UE)
+# - Object-Relational Mapping (ORM) (12 UE)
+
+# %% [markdown]
+#
+# ### Woche 21: Einführung in NoSQL-Datenbanken
+#
+# - Überblick über NoSQL-Datenbanken (6 UE)
+# - Verschiedene NoSQL-Datenbanktypen (34 UE)
+
+# %% [markdown]
+#
+# ## Service-Orientierte Architektur (80 UE)
+#
+# ### Woche 22: Überblick über Container, Orchestrierung und Microservices
+#
+# - Containerisierung mit Docker (12 UE)
+# - Einführung in Microservices (12 UE)
+# - API-Entwicklung (16 UE)
+
+# %% [markdown]
+#
+# ### Woche 23: Microservices-Entwicklung und -Deployment
+#
+# - Implementierung von Microservices (32 UE)
+# - Deployment von Microservices (8 UE)
+
+# %% [markdown]
+#
+# ## Softwaresicherheit und ethische Aspekte (40 UE)
+#
+# ### Woche 24: Grundlagen der Softwaresicherheit und Ethische Aspekte
+#
+# - Sicherheitsgrundlagen und häufige Angriffe (8 UE)
+# - Sichere Codierung und Best Practices (12 UE)
+# - Kryptographie und Datenschutz (14 UE)
+# - Ethische Grundlagen und Verantwortung (6 UE)
+
+# %% [markdown]
+#
+# **Unterrichtseinheiten insgesamt: 960**
+
