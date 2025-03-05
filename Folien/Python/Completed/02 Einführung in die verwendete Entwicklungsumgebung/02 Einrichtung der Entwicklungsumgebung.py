7c57d403f08cb282f0dd4281d6452d4ce87c630c
Matthias Hölzl <tc@xantira.com>
2025-03-05 01:22:11 +0100

    Update files for day 3

--- Folien/Python/Completed/02 Einführung in die verwendete Entwicklungsumgebung/02 Einrichtung der Entwicklungsumgebung.py
+++ Folien/Python/Completed/02 Einführung in die verwendete Entwicklungsumgebung/02 Einrichtung der Entwicklungsumgebung.py
@@ -9,6 +9,25 @@
 
 # %% [markdown]
 #
+# ## Einrichten von Python
+#
+# In einem PowerShell-Fenster:
+#
+# - `pip install jupyterlab pytest`
+
+# %% [markdown]
+#
+# ## Einrichten von Visual Studio Code
+#
+# - Installation der [Python Erweiterung](https://marketplace.visualstudio.com/items?itemName=ms-python.python)
+# - Installation der [Jupyter Erweiterung](https://marketplace.visualstudio.com/items?itemName=ms-toolsai.jupyter)
+# - Optional:
+#   - Installation von Keybinding-Erweiterungen wie [Visual Studio](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vs-keybindings)
+#   - Anpassen/Installation von [Color Themes](https://code.visualstudio.com/docs/getstarted/themes)
+#   - ...
+
+# %% [markdown]
+#
 # ## Einrichten von Git
 #
 # In einem PowerShell-Fenster:
@@ -17,21 +36,3 @@
 # - `git config --global user.email max.mustermann@example.de`
 #
 # (Ersetzen Sie Namen und E-Mail-Adresse durch Ihre eigenen)
-
-# %% [markdown]
-#
-# ## Einrichten von Python
-#
-# In einem PowerShell-Fenster:
-#
-# - `pip install jupyterlab`
-
-# %% [markdown]
-#
-# ## Einrichten von Visual Studio Code
-#
-# - Installation der [Python Erweiterung](https://marketplace.visualstudio.com/items?itemName=ms-python.python)
-# - Optional:
-#   - Installation von Keybinding-Erweiterungen wie [Visual Studio](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vs-keybindings)
-#   - Anpassen/Installation von [Color Themes](https://code.visualstudio.com/docs/getstarted/themes)
-#   - ...
--- Folien/Python/Completed/02 Einführung in die verwendete Entwicklungsumgebung/03 Erstellung eines ersten Projekts.py
+++
@@ -1,8 +0,0 @@
-# %% [markdown]
-#
-# <div style="text-align:center; font-size:200%;">
-#  <b>Erstellung eines ersten Projekts</b>
-# </div>
-# <br/>
-# <div style="text-align:center;">Dr. Matthias Hölzl</div>
-# <br/>
