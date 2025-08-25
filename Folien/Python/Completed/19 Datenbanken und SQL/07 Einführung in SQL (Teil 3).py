0635a5f402d8725119037460fbf660df69eb7586
Matthias Hölzl <tc@xantira.com>
2025-08-25 05:52:55 +0200

    Improve DB slides

--- Folien/Python/Completed/19 Datenbanken und SQL/07 Einführung in SQL (Teil 3).py
+++ Folien/Python/Completed/19 Datenbanken und SQL/07 Einführung in SQL (Teil 3).py
@@ -9,13 +9,13 @@
 
 # %% [markdown]
 #
-# ## Abrufen von Werten
+# ## `SELECT` Abfragen
 #
-# - `SELECT`:
-#   - Der SELECT-Befehl wird verwendet, um Daten aus einer oder mehreren
-#     Tabellen in einer Datenbank abzurufen
-#   - Man kann angeben, welche Spalten abgerufen werden sollen, sowie
-#     Bedingungen für Filterung der Ergebnisse angeben
+# - Mit SELECT Abfragen kann manDaten aus einer oder mehreren Tabellen in einer
+#   Datenbank abzurufen
+# - Man kann angeben, welche Spalten abgerufen werden sollen, sowie Bedingungen
+#   für Filterung der Ergebnisse angeben
+# - Das Ergebnis der Abfrage ist eine Tabelle mit den angeforderten Daten
 
 
 # %% [markdown]
@@ -24,24 +24,27 @@
 # SELECT * FROM customers;
 # ```
 
+# %% [markdown]
+#
+# ```sql
+# SELECT name, address FROM customers;
+# ```
+
 
 # %% [markdown]
 #
-# - `FROM`:
-#   - Die `FROM`-Klausel gibt an, aus welchen Tabellen Daten abgerufen werden
-#     sollen.
-#   - Man kann Daten aus mehreren Tabellen abrufen, indem man sie miteinander
-#     verbindet
+# ### Die `FROM`-Klausel
+#
+# - Die `FROM`-Klausel gibt an, aus welchen Tabellen Daten abgerufen werden
+#   sollen.
+#   - Das kann eine Tabelle aus der Datenbank, oder das Ergebnis einer
+#     `SELECT`-Abfrage sein.
+# - Man kann Daten aus mehreren Tabellen abrufen, indem man sie miteinander
+#   verbindet.
 
 # %% [markdown]
 # ```sql
 # SELECT * FROM customers;
-# ```
-
-# %% [markdown]
-#
-# ```sql
-# SELECT (name, address) FROM customers;
 # ```
 
 # %% [markdown]
@@ -52,15 +55,15 @@
 # %% [markdown]
 #
 # - `WHERE`:
-# - Die `WHERE`-Klausel wird verwendet, um die Ergebnisse einer
-#   `SELECT`-Anweisung zu filtern
-# - Man kann Vergleichsoperatoren (z.B.  `=`, `<`, `>`) und logische Operatoren
-#   (z.B. `AND`, `OR`, `NOT`) verwenden, um die Bedingungen zu spezifizieren
+#   - Die `WHERE`-Klausel wird verwendet, um die Ergebnisse einer
+#     `SELECT`-Anweisung zu filtern
+#   - Man kann Vergleichsoperatoren (z.B.  `=`, `<`, `>`) und logische Operatoren
+#     (z.B. `AND`, `OR`, `NOT`) verwenden, um die Bedingungen zu spezifizieren
 
 # %% [markdown]
 #
 # ```sql
-# SELECT * FROM customers where age > 18;
+# SELECT * FROM orders where quantity >= 2;
 # ```
 
 # %% [markdown]
@@ -69,11 +72,138 @@
 # SELECT * FROM customers, orders WHERE customers.id = orders.customer_id;
 # ```
 
+
+# %% [markdown]
+# ```sql
+# SELECT customers.id, orders.id
+#   FROM customers, orders
+#   WHERE customers.id = orders.customer_id;
+# ```
+
+# %% [markdown]
+# ```sql
+# SELECT customers.id AS customer_id, orders.id AS order_id
+#   FROM customers, orders
+#   WHERE customers.id = orders.customer_id;
+# ```
+
+
+# %% [markdown]
+# ```sql
+# SELECT customers.id AS customer_id, orders.id AS order_ id
+# FROM customers
+# JOIN orders ON customers.id = orders.customer_id;
+# ```
+
+
 # %% [markdown]
 #
 # ```sql
-# SELECT (name, address, product_name, quantity)
+# SELECT name, address, product_name, quantity, price
 #   FROM customers, orders, products
 #   WHERE customers.id = orders.customer_id
 #   AND products.id = orders.product_id;
+# ```
+
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, address, product_name, quantity, price, quantity * price AS total_price
+#   FROM customers, orders, products
+#   WHERE customers.id = orders.customer_id
+#   AND products.id = orders.product_id;
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, address, product_name, quantity, price, quantity * price AS total_price
+#   FROM customers
+#   JOIN orders ON customers.id = orders.customer_id
+#   JOIN products ON products.id = orders.product_id;
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, total_price
+#   FROM (SELECT customer_id, quantity * price AS total_price
+#     FROM orders
+#     JOIN products ON products.id = orders.product_id) AS subquery;
+#   JOIN customers ON customers.id = subquery.customer_id;
+# ```
+
+
+# %% [markdown]
+#
+# - `DISTINCT`:
+#   - Der `DISTINCT`-Operator wird verwendet, um doppelte Werte aus den Ergebnissen
+#     einer `SELECT`-Anweisung zu entfernen
+
+# %% [markdown]
+#
+# ```sql
+# SELECT DISTINCT name, address
+#   FROM customers, orders
+#   WHERE customers.id = orders.customer_id;
+# ```
+
+
+# %% [markdown]
+#
+# ## Workshop: SQL-Abfragen
+#
+# - Fügen Sie zur `customers`-Tabelle einige neue Kunden hinzu
+# - Fügen Sie zur `products`-Tabelle einige neue Produkte hinzu
+# - Erstellen Sie SQL-Abfragen, die folgende Ergebnisse zurückgeben:
+#   1. Die Namen und Adressen aller Kunden, die Bestellungen aufgegeben haben
+#   2. Die Namen und Preise aller Produkte, die verkauft wurden
+#   3. Den ID und den Gesamtpreis jeder Bestellung des Kunden mit der (Kunden-)ID 1
+
+# %% [markdown]
+#
+# ```sql
+# INSERT INTO customers (name, address) VALUES ('David Parker', '587 Oak St');
+# INSERT INTO customers (name, address) VALUES ('Emma Wilson', '123 Maple Ave');
+# INSERT INTO products (product_name, price) VALUES ('Widget', 19.99);
+# INSERT INTO products (product_name, price) VALUES ('Gadget', 29.99);
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT DISTINCT name, address
+#   FROM customers, orders
+#   WHERE customers.id = orders.customer_id;
+# -- Alternative
+# SELECT DISTINCT name, address
+#   FROM customers
+#   WHERE id IN (SELECT customer_id FROM orders);
+# ```
+
+# %% [markdown]
+#
+# Die zweite Variante (Subquery) erlaubt es uns, Produkte zu finden, die nicht
+# bestellt wurden:
+
+# %% [markdown]
+#
+# ```sql
+# SELECT DISTINCT product_name, price
+#   FROM products, orders
+#   WHERE products.id = orders.product_id;
+# -- Alternative
+# SELECT DISTINCT product_name, price
+#   FROM products
+#   WHERE id IN (SELECT product_id FROM orders);
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT orders.id, quantity * price AS total_price
+#   FROM orders, products
+#   WHERE orders.product_id = products.id
+#   AND orders.customer_id = 1
 # ```
--- Folien/Python/Completed/19 Datenbanken und SQL/08 Einführung in SQL (Teil 4).py
+++ Folien/Python/Completed/19 Datenbanken und SQL/08 Einführung in SQL (Teil 4).py
@@ -17,6 +17,16 @@
 
 # %% [markdown]
 #
+# ```sql
+# SELECT name, SUM(quantity * price) AS total_price
+#   FROM customers, orders, products
+#   WHERE customers.id = orders.customer_id
+#   AND products.id = orders.product_id
+#   GROUP BY name;
+# ```
+
+# %% [markdown]
+#
 # - `ORDER BY`:
 # - Die `ORDER BY`-Klausel wird verwendet, um die Ergebnisse einer
 #   `SELECT`-Anweisung nach einer oder mehreren Spalten zu sortieren
@@ -28,3 +38,49 @@
 # - `LIMIT`:
 # - Die `LIMIT`-Klausel wird verwendet, um die Anzahl der Ergebnisse , die von
 #   einer `SELECT` Anweisung zurückgegeben werden, zu begrenzen
+
+# %% [markdown]
+#
+# ## Workshop: SQL-Abfragen
+#
+# - Fügen Sie zur `customers`-Tabelle einige neue Kunden hinzu
+# - Fügen Sie zur `products`-Tabelle einige neue Produkte hinzu
+# - Erstellen Sie SQL-Abfragen, die folgende Ergebnisse zurückgeben:
+#   1. Den Namen und Gesamtumsatz eines bestimmten Kunden
+#   2. Die Namen und den Umsatz aller Kunden, die eine Bestellung aufgegeben
+#      haben
+#   3. Die Namen der drei Kunden mit dem höchsten Gesamtumsatz
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, SUM(quantity * price) AS total_sales
+#   FROM customers, orders, products
+#   WHERE customers.id = orders.customer_id
+#   AND products.id = orders.product_id
+#   AND customers.id = 1
+#   GROUP BY name;
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, SUM(quantity * price) AS total_sales
+#   FROM customers, orders, products
+#   WHERE customers.id = orders.customer_id
+#   AND products.id = orders.product_id
+#   GROUP BY name
+#   ORDER BY total_sales DESC;
+# ```
+
+# %% [markdown]
+#
+# ```sql
+# SELECT name, SUM(quantity * price) AS total_sales
+#   FROM customers, orders, products
+#   WHERE customers.id = orders.customer_id
+#   AND products.id = orders.product_id
+#   GROUP BY name
+#   ORDER BY total_sales DESC
+#   LIMIT 3;
+# ```
